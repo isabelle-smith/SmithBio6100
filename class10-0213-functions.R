88859952204b2b299e884993ed1130b7556aedf9
SmizzPad <penguinjaune@yahoo.com>
2025-02-13 13:50:40 -0500

    2024.02.13

---
+++ class10-0213-functions.R
@@ -0,0 +1,99 @@
+
+## BIOL6100 - Class 13
+## Isabelle Smith
+## Feb. 12, 2025
+
+## - - - - - - -
+
+## intro to R viii
+
+## - - - - - - -
+
+
+
+## Scoping In Functions
+## - - - - - - -
+
+## - global variables: visible to all parts of the code; declared in main body
+## - local variables: visible only within function; declared in function or passed to function through parameters
+## - functions “can” see variables in global environment
+## - global environment cannot see variables in function environment
+
+my_func <- function(a=3,b=4) {
+  z <- a + b
+  return(z)
+}
+my_func()
+
+my_func_bad <-function(a=3) {
+  z <- a + b
+  return(z)
+}
+my_func_bad() # crashes because it can't find b
+b <- 100
+my_func_bad() # OK now because b exists as a global variable
+
+# But it is fine to create variables locally
+my_func_ok <- function(a=3) {
+  bb <- 100
+  z <- a + bb
+  return(z)
+}
+
+my_func_ok() # no problems now
+print(bb) # but this variable is still hidden from the global environment
+
+## - - - - - - -
+
+
+
+## Simple regression function
+## - - - - - - -
+
+##################################################
+# FUNCTION: fitLinear 
+# fits simple regression line
+# inputs: numeric vector of predictor (x) and response (y)
+# outputs: slope and p-value
+#------------------------------------------------- 
+fitLinear <- function(x=runif(20),y=runif(20)) {
+  myMod <- lm(y~x) # fit the model
+  myOut <- c(slope=summary(myMod)$coefficients[2,1],
+             pValue=summary(myMod)$coefficients[2,4])
+  plot(x=x,y=y) # quick and dirty plot to check output
+  return(myOut)
+}
+##################################################
+fitLinear()
+
+## - - - - - - -
+
+
+
+## Creating a more complex default value
+## - - - - - - -
+
+##################################################
+# FUNCTION: fitLinear2       
+# fits simple regression line
+# inputs: numeric vector of predictor (x) and response (y)
+# outputs: slope and p-value
+#------------------------------------------------- 
+fitLinear2 <- function(p=NULL) {
+  if(is.null(p)) {
+    p <- list(x=runif(20),y=runif(20))
+  }
+  myMod <- lm(p$y~p$x) # fit the model                    ## FIXED
+  myOut <- c(slope=summary(myMod)$coefficients[2,1],
+             pValue=summary(myMod)$coefficients[2,4])
+  plot(x=p$x,y=p$y) # quick and dirty plot to check output
+  return(myOut)
+}
+
+##################################################
+fitLinear2()
+myPars <-list(x=1:10,y=sort(runif(10)))             ## CHANGED
+fitLinear2(myPars)
+
+## - - - - - - -
+
