e2755f2ac06fbb6acd614175cd458d073f0689f9
SmizzPad <penguinjaune@yahoo.com>
2025-04-15 20:59:25 -0400

    2025.04.15

---
+++ filebatchr/Functions/CreateToyDataFiles.R
@@ -0,0 +1,34 @@
+# --------------------------------------
+# FUNCTION create_toy_data_files
+# required packages: none
+# description: build a set of toy csv files
+# inputs: number of files, max rows, max columns
+# outputs: a set of .csv files in a new subfolder
+########################################
+create_toy_data_files <- function(nrow=NULL,
+                                  ncol=NULL,
+                                  nfiles=NULL){
+
+  # assign parameter defaults
+  if (is.null(nrow) | is.null(ncol) | is.null(nfiles)) {
+    nrow=10
+    ncol=10
+    nfiles=15
+  }
+  
+  # function body
+  
+  for (i in 1:nfiles){
+    df <- as.data.frame(matrix(runif(nrow*ncol), nrow=nrow))
+    
+    file_labels <- create_padded_labels(n=nfiles, string="Toy_Data", suffix=".csv")
+    
+    write.table(df, file=paste("CleanedData/ToyDataFiles/", file_labels[i], sep=""))
+  
+  }
+  
+  #l('...checking function: create_toy_data_files()')
+
+} # end of function create_toy_data_files
+# --------------------------------------
+create_toy_data_files()
---
+++ filebatchr/MainScript.R
@@ -0,0 +1,9 @@
+
+# filebatchr() for operating on batch files
+# Biol 6100 Spring 2025
+# started Apr. 15, 2025 (class 24)
+
+library(upscaler)
+library(ggplot2)
+
+set_up_log()
